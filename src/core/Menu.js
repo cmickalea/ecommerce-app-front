9f84ad57ab05b363f5f7fcb6bcc8990bb03187cd
Mickalea Chambers <iamchambers4@gmail.com>
2020-07-01 11:26:13 -0400

    added signout li to menu

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { signout } from "../auth";
 
 const isActive = (history, path) => {
     if(history.location.pathname === path){
@@ -19,8 +20,24 @@ const Menu = ({history}) => {
           <ul className="nav nav-tabs bg-primary">
               <li className="nav-item">
                   <Link className="nav-link" style={isActive(history, "/")} to="/">Home</Link>
+              </li>
+
+              <li>
                   <Link className="nav-link" style={isActive(history, "/signin")} to="/signin">Signin</Link>
+              </li>
+
+              <li>
                   <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Signup</Link>
+              </li>
+
+              <li>
+                  <span className="nav-link"
+                        style={{cursor: "pointer", color:"#fff"}}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                             })
+                        }>Signout</span>
               </li>
           </ul>
       </div>
