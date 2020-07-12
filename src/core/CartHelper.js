c811e197e94d1c5c380f2193926fcf4994046b1f
Mickalea Chambers <iamchambers4@gmail.com>
2020-07-12 12:25:21 -0400

    added methods to create array of items and to return item total

---
+++ src/core/CartHelper.js
@@ -0,0 +1,33 @@
+export const addItem = (item, next) => {
+    let cart = [];
+    if(typeof window !== "undefined"){
+        if(localStorage.getItem("cart")){
+            cart = JSON.parse(localStorage.getItem("cart"));
+        }
+        cart.push({
+            ...item,
+            count: 1
+        })
+
+        // creates new array of cart items and new Set removes the duplicates
+        // new Set only allows unique values
+        // take the array of product ids and re-map it to return the actual product
+
+        cart = Array.from(new Set(cart.map(p => p._id))).map(id => {
+            return cart.find(p => p._id === id);
+        })
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        next();
+    }
+}
+
+
+export const itemTotal = () => {
+    if(typeof window !== "undefined"){
+        if(localStorage.getItem("cart")){
+            return JSON.parse(localStorage.getItem("cart")).length
+        }
+    }
+    return 0;
+}
