d8f22b1caecb799a6ad6469418444685962f55f8
Mickalea Chambers <iamchambers4@gmail.com>
2020-07-12 12:43:00 -0400

    created Cart page which displays products added to cart

---
+++ src/core/Cart.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import {Link} from "react-router-dom";
+import Layout from "./Layout";
+import { getCart } from "./CartHelper";
+import Card from "./Card";
+
+const Cart = () => {
+    const [ items, setItems] = useState([]);
+
+    useEffect(() => {
+        setItems(getCart())
+    }, []);
+
+    const showItems = items => {
+        return (
+            <div>
+                <h2>Your cart has {`${items.length}`} items</h2>
+                <hr/>
+                {items.map((product, i) => (<Card key={i} product={product} />))}
+            </div>
+        )
+    }
+
+    const emptyCartMessage = () => (
+        <h2>Nada . Zilch . Nothing Here Brokey... Go <Link to="/shop">Shop</Link> </h2>
+    )
+
+    return (
+        <Layout title="Shopping Cart" description="Manage Your Cart" className="container-fluid">
+            <div className="row">
+                <div className="col-6">
+                    {items.length > 0 ? showItems(items) : emptyCartMessage()}
+                </div>
+
+                <div className="col-6">
+                    <p> Checkout </p>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Cart;
