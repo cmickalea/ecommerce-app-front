df7bf3a7b5f3e0d2adf31605e279794bd39bf6b2
Mickalea Chambers <iamchambers4@gmail.com>
2020-07-12 12:23:10 -0400

    added cart link to nav

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,8 @@
 import React, { Fragment} from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./CartHelper";
+import "../style.css";
 
 const isActive = (history, path) => {
     if(history.location.pathname === path){
@@ -21,8 +23,19 @@ const Menu = ({history}) => {
               <li className="nav-item">
                   <Link className="nav-link" style={isActive(history, "/")} to="/">Home</Link>
               </li>
+
               <li className="nav-item">
                   <Link className="nav-link" style={isActive(history, "/shop")} to="/shop">Shop</Link>
+              </li>
+
+              <li className="nav-item">
+                  <Link
+                      className="nav-link"
+                      style={isActive(history, "/cart")}
+                      to="/cart"
+                  >
+                      Cart <sup><small className="cart-badge">{itemTotal()}</small></sup>
+                  </Link>
               </li>
 
               {isAuthenticated() && isAuthenticated().user.role === 0 && (
